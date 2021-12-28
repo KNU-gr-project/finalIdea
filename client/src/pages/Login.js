724da370ff0c85808016218f5cd1c2c7fa80d21a
Maru <123rudgks@gmail.com>
2021-12-28 21:58:36 +0900

    pull request test2

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -9,6 +9,7 @@ import Swal from "sweetalert";
 //임시 import
 // import { KAKAO_AUTH_URL } from "./OAuth";
 // 이렇게 fork를 하는게 맞나?
+// 아니 뭐냐고
 async function loginUser(data) {
   // const history = useHistory();
   axios
